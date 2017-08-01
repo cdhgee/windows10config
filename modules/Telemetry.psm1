4f09f1cbb47a916bf48a71ebaac32492c21d71fd
David Gee <git@allpowerfuldave.com>
2017-08-01 15:56:09 -0500

    Add telemetry module

---
+++ modules/Telemetry.psm1
@@ -0,0 +1,19 @@
+﻿Function Get-Description {
+
+  [CmdletBinding()]
+  Param()
+
+  "Disabling telemetry"
+
+}
+
+Function Set-Configuration {
+
+  [CmdletBinding()]
+  Param()
+
+  Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
+  Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
+  Set-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Policies\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
+  
+}
