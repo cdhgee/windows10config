9a72d7cc3d00dc189d5668c7158be3d4b5cc8f62
David Gee <git@allpowerfuldave.com>
2017-08-01 16:04:46 -0500

    Add privacy module

---
+++ modules/Privacy.psm1
@@ -0,0 +1,43 @@
+﻿Function Get-Description {
+
+  [CmdletBinding()]
+  Param()
+
+  "Applying privacy settings"
+
+}
+
+Function Set-Configuration {
+
+  [CmdletBinding()]
+  Param()
+
+  # Disable Wi-Fi Sense
+  Write-Host "Disabling Wi-Fi Sense..."
+  If (!(Test-Path "HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting")) {
+      New-Item -Path "HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting" -Force | Out-Null
+  }
+  Set-ItemProperty -Path "HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting" -Name "Value" -Type DWord -Value 0
+  Set-ItemProperty -Path "HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowAutoConnectToWiFiSenseHotspots" -Name "Value" -Type DWord -Value 0
+
+  # Disable SmartScreen Filter
+  Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer" -Name "SmartScreenEnabled" -Type String -Value "Off"
+  Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\AppHost" -Name "EnableWebContentEvaluation" -Type DWord -Value 0
+
+  # Disable Location Tracking
+  Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Sensor\Overrides\{BFA794E4-F964-4FDB-90F6-51056BFE4B44}" -Name "SensorPermissionState" -Type DWord -Value 0
+  Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Services\lfsvc\Service\Configuration" -Name "Status" -Type DWord -Value 0
+
+  # Disable Feedback
+  If (!(Test-Path "HKCU:\Software\Microsoft\Siuf\Rules")) {
+      New-Item -Path "HKCU:\Software\Microsoft\Siuf\Rules" -Force | Out-Null
+  }
+  Set-ItemProperty -Path "HKCU:\Software\Microsoft\Siuf\Rules" -Name "NumberOfSIUFInPeriod" -Type DWord -Value 0
+
+  # Disable Advertising ID
+  If (!(Test-Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\AdvertisingInfo")) {
+      New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\AdvertisingInfo" | Out-Null
+  }
+  Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\AdvertisingInfo" -Name "Enabled" -Type DWord -Value 0
+}
+
