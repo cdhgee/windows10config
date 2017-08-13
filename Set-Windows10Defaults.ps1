2e052d78e997b2ca9e8bdb31dedbc7e4368e1a30
David Gee <git@allpowerfuldave.com>
2017-08-13 13:13:30 -0500

    Add main script

---
+++ Set-Windows10Defaults.ps1
@@ -0,0 +1,38 @@
+﻿[CmdletBinding()]
+Param()
+
+Function main {
+
+  $script:basepath = (Get-Location).Path
+  $modules = Get-ChildItem -Path "$($script:basepath)/modules" -Filter "*.psm1"
+
+  Foreach($module in $modules){
+    Import-Module -Name $module.FullName -Prefix $module.BaseName
+  }
+
+  Foreach($module in $modules){
+    $msg = Invoke-ModuleFunction -Module $module.Basename -Function "Get-Description"
+    Write-Host $msg
+  }
+  
+
+}
+
+Function Invoke-ModuleFunction {
+
+  [CmdletBinding()]
+  Param(
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    $Module,
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    $Function
+  )
+
+  $verb, $rest = $Function -split "-"
+  $reststring = $rest -join "-"
+  & "$verb-$Module$reststring"
+}
+
+main
