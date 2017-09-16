dc804baf460259ee10045673684450259aec1935
David Gee <git@allpowerfuldave.com>
2017-09-16 13:31:29 -0500

    Add module to remove default apps

---
+++ modules/DefaultApps.psm1
@@ -0,0 +1,18 @@
+﻿Function Get-Description {
+
+  [CmdletBinding()]
+  Param()
+
+  "Removing default apps"
+
+}
+
+Function Set-Configuration {
+
+  [CmdletBinding()]
+  Param()
+
+  $applist = [xml](Get-Content -Path "$PSScriptRoot/config/default-apps.xml" -Encoding utf8)
+  $applist."default-apps".app.name | Get-AppXPackage | Remove-AppXPackage
+}
+
